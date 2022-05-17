a87eace7bd3ced25669728caaaa04cc6bce23c6d
jasony62 <jasony62@qq.com>
2022-05-17 12:30:15 +0800

    优化代码

--- packages/tms-vue3-ui/test/json-doc/object-map-array.test.ts
+++ packages/tms-vue3-ui/test/json-doc/object-map-array.test.ts
@@ -1,8 +1,8 @@
 import { SampleSchema } from '../data/schema-object-map-array'
 import { build } from '../../src/json-doc/builder'
 
-describe('通过API从外部获取值', () => {
-  it('返回字段的值', () => {
+describe('对象包含可选属性，可选属性为数组类型', () => {
+  it('生成表单节点', () => {
     const editDoc = {
       org: {
         name: '研发部',
@@ -10,21 +10,28 @@ describe('通过API从外部获取值', () => {
         arrXyz: [{ label: 'xxx', value: '999' }],
       },
     }
-    const nodes = build({
-      schema: SampleSchema,
-      editDoc,
-      onMessage: (msg: string) => {
-        console.log(msg)
+    const fieldNames: string[] = []
+    build(
+      {
+        schema: SampleSchema,
+        editDoc,
+        onMessage: (msg: string) => {
+          console.log(msg)
+        },
       },
-    })
-    /**
-     * 1 根节点，div
-     * 1.1 嵌套节点，div
-     * 1.1.1 标题节点 div
-     * 1.1.2 字段节点 div
-     * 1.1.2.1 time
-     * 1.1.2.2 content
-     */
-    //console.log(JSON.stringify(nodes, null, 2))
+      fieldNames
+    )
+    // console.log(fieldNames)
+    expect(fieldNames).toHaveLength(10)
+    expect(fieldNames[0]).toBe('org.name')
+    expect(fieldNames[1]).toBe('org.arrXyz[0].label')
+    expect(fieldNames[2]).toBe('org.arrAbc[0].label')
+    expect(fieldNames[3]).toBe('org.arrXyz[0].value')
+    expect(fieldNames[4]).toBe('org.arrAbc[0].value')
+    expect(fieldNames[5]).toBe('org.arrXyz[0]')
+    expect(fieldNames[6]).toBe('org.arrXyz')
+    expect(fieldNames[7]).toBe('org.arrAbc[0]')
+    expect(fieldNames[8]).toBe('org.arrAbc')
+    expect(fieldNames[9]).toBe('org')
   })
 })
---
+++ packages/tms-vue3-ui/test/json-doc/object-map-object.test.ts
@@ -0,0 +1,44 @@
+import { SampleSchema } from '../data/schema-object-map'
+import { build } from '../../src/json-doc/builder'
+
+describe('对象包含可选属性，可选属性为对象类型', () => {
+  it('生成表单节点', () => {
+    const editDoc = {
+      org: {
+        name: '研发部',
+        strProduct: 'tms-vue3-kit',
+        'str in valid': '123',
+        objAbc: { label: 'aaa', value: '111' },
+        objXyz: { label: 'xxx', value: '999' },
+      },
+    }
+    const fieldNames: string[] = []
+    build(
+      {
+        schema: SampleSchema,
+        editDoc,
+        onMessage: (msg: string) => {
+          console.log(msg)
+        },
+      },
+      fieldNames
+    )
+    // console.log(fieldNames)
+    expect(fieldNames).toHaveLength(15)
+    expect(fieldNames[0]).toBe('org.name')
+    expect(fieldNames[1]).toBe('org.strProduct')
+    expect(fieldNames[2]).toBe('org.objXyz.label')
+    expect(fieldNames[3]).toBe('org.objAbc.label')
+    expect(fieldNames[4]).toBe('org.objXyz.value')
+    expect(fieldNames[5]).toBe('org.objAbc.value')
+    expect(fieldNames[6]).toBe('org.objAbc.extra.label')
+    expect(fieldNames[7]).toBe('org.objXyz.extra.label')
+    expect(fieldNames[8]).toBe('org.objAbc.extra.value')
+    expect(fieldNames[9]).toBe('org.objXyz.extra.value')
+    expect(fieldNames[10]).toBe('org.objAbc.extra')
+    expect(fieldNames[11]).toBe('org.objXyz.extra')
+    expect(fieldNames[12]).toBe('org.objXyz')
+    expect(fieldNames[13]).toBe('org.objAbc')
+    expect(fieldNames[14]).toBe('org')
+  })
+})
