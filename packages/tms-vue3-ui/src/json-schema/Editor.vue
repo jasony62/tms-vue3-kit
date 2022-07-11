173fb1391a2420ee4971849c286686c2e96326c5
DESKTOP-MP4H2RS\baiss <2764411477@qq.com>
2022-07-11 10:11:33 +0800

    完善切换选项模式时的逻辑

--- packages/tms-vue3-ui/src/json-schema/Editor.vue
+++ packages/tms-vue3-ui/src/json-schema/Editor.vue
@@ -94,6 +94,7 @@
       <tvu-form-item class="tvu-jse__field__actions">
         <tvu-button @click="onRemoveNode">删除属性</tvu-button>
         <tvu-button @click="onAddNode" v-if="hasAddNode">添加属性</tvu-button>
+        <tvu-button @click="onAddNode" v-if="hasAddNode">添加模板属性</tvu-button>
       </tvu-form-item>
     </div>
   </div>
@@ -218,18 +219,36 @@ const onChangeItemsFormat = (event: any) => {
 
 const onChangeChoiceMode = (event: any) => {
   const { attrs } = data.currProp
-  const value = event.target.value
-  if (value) {
-    if (!Array.isArray(attrs[value])) {
-      attrs[value] = [
+  let oldValue = ""
+  Object.keys(attrs).forEach(item => {
+    if (['oneOf', 'anyOf', 'enum'].includes(item)) {
+      oldValue = item
+    }
+  })
+  const newValue = event.target.value
+
+  if (oldValue) {
+    if (!newValue) {
+      delete attrs[oldValue]
+      delete attrs.enumGroups
+    } else if (newValue !== oldValue) {
+      delete attrs[oldValue]
+      delete attrs.enumGroups
+
+      attrs[newValue] = [
         { label: '选项1', value: 'a' },
-        { label: '选项2', value: 'b' },
+        { label: '选项2', value: 'b' }
       ]
       data.currProp.attrs.enumGroups = []
     }
   } else {
-    delete attrs[value]
-    delete attrs.enumGroups
+    if (newValue) {
+      attrs[newValue] = [
+        { label: '选项1', value: 'a' },
+        { label: '选项2', value: 'b' }
+      ]
+      data.currProp.attrs.enumGroups = []
+    }
   }
 }
 
