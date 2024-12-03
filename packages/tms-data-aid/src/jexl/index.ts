a031290e63ca0ab04e0494d05bfe134152d6a331
jasony62 <jasony62@qq.com>
2024-12-03 13:27:05 +0800

    优化代码

--- packages/tms-data-aid/src/jexl/index.ts
+++ packages/tms-data-aid/src/jexl/index.ts
@@ -1,4 +1,53 @@
 import Jexl from 'jexl'
+
+Jexl.addTransform('keys', (val) =>
+  val && typeof val === 'object' ? Object.keys(val) : []
+)
+
+Jexl.addTransform('values', (val) =>
+  val && typeof val === 'object' ? Object.values(val) : []
+)
+
+Jexl.addTransform('parseInt', (val) =>
+  Array.isArray(val) ? val.map((v) => parseInt(v)) : parseInt(val)
+)
+
+Jexl.addTransform('parseFloat', (val) =>
+  Array.isArray(val) ? val.map((v) => parseFloat(v)) : parseFloat(val)
+)
+
+Jexl.addTransform('length', (val) => val?.length ?? 0)
+
+Jexl.addTransform('sum', (val: number[]) =>
+  Array.isArray(val) ? val.reduce((a: number, b: number) => a + b, 0) : 0
+)
+
+Jexl.addTransform('max', (val: number[]) =>
+  Array.isArray(val) ? Math.max(...val) : 0
+)
+
+Jexl.addTransform('min', (val: number[]) =>
+  Array.isArray(val) ? Math.min(...val) : 0
+)
+
+Jexl.addTransform('min', (val: number[]) =>
+  Array.isArray(val) ? val.reduce((a: number, b: number) => a + b, 0) : 0
+)
+
+Jexl.addTransform('avg', (val: number[]) =>
+  Array.isArray(val)
+    ? val.reduce((a: number, b: number) => a + b, 0) / val.length
+    : 0
+)
+
+Jexl.addFunction('padRightToLength', (arr, maxLength = 0, padChar = 0) => {
+  if (!Array.isArray(arr)) return arr
+  if (arr.length >= maxLength) return arr
+  return [
+    ...arr,
+    ...Array.from({ length: maxLength - arr.length }, () => padChar),
+  ]
+})
 /**
  * 获取数组长度
  */
