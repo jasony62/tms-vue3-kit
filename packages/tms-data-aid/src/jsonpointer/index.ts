e091f8686d26ac13deb75ddd2950764b4a8cf276
jasony62 <jasony62@qq.com>
2025-01-02 23:30:43 +0800

    优化代码

--- packages/tms-data-aid/src/jsonpointer/index.ts
+++ packages/tms-data-aid/src/jsonpointer/index.ts
@@ -1,4 +1,5 @@
 import JSONPointer from 'jsonpointer'
+import lodashSet from 'lodash.set'
 
 /**
  * lodash兼容的路径
@@ -11,17 +12,22 @@ class JSONPointerLodash {
    * @param value
    */
   static set(object: Object, dotPath: string, value: any) {
-    let pointer = '/' + dotPath.replace(/\.\[(\d+)\]/, '/$1').replace('.', '/')
-    /**
-     * 检查父节点是否为undefined或null，如果是则删除
-     */
-    let parts = pointer.split('/')
-    if (parts.length > 2) {
-      let pp = parts.slice(0, -1).join('/')
-      let pv = JSONPointer.get(object, pp)
-      if (pv ?? true) JSONPointer.set(object, pp, undefined)
-    }
-    JSONPointer.set(object, pointer, value)
+    // let pointer =
+    //   '/' + dotPath.replaceAll(/\[(\d+)\]/g, '/-').replaceAll('.', '/')
+    // /**
+    //  * 检查父节点是否为undefined或null，如果是则删除
+    //  */
+    // let parts = pointer.split('/')
+    // if (parts.length > 2) {
+    //   let pp = parts.slice(0, -1).join('/')
+    //   let pv = JSONPointer.get(object, pp)
+    //   if (pv === undefined || pv === null)
+    //     if (/\/-$/.test(pp)) {
+    //       JSONPointer.set(object, pp, [])
+    //     } else JSONPointer.set(object, pp, undefined)
+    // }
+    // JSONPointer.set(object, pointer, value)
+    lodashSet(object, dotPath, value)
   }
 }
 
